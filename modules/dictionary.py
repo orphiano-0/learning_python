4172d5e28b9e451d56f46706331c21fe81a91d74
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-06-28 22:22:11 +0800

    Adding new modules and learnings

---
+++ modules/dictionary.py
@@ -0,0 +1,8 @@
+nicknameLists = {
+    "Gab": "Gabriel",
+    "Art": "Arthur",
+    "Tess": "Tessia",
+    "Kath": "Kathlyn"
+}
+#it will print the value of a key.
+print(nicknameLists["Gab"])
---
+++ modules/loop.py
@@ -0,0 +1,10 @@
+#--WHILE LOOP
+#creating variables and conditions
+import time
+
+number = 1 #value
+while number <= 5: #condition
+    print(number)
+    time.sleep(2.3) #not necessary, just for some cool countdown.
+    number += 1 #iteration
+print("Done")
