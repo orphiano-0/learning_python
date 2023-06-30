ef4383e910b77c038419da3946c958a0262f7c3d
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-06-30 12:08:36 +0800

    Reading files and try except block.

--- modules/file_manipulation.py
+++ modules/file_manipulation.py
@@ -1,6 +1,19 @@
-database = open("../files/database.txt", "r")
-for data in database.readlines():
-    print(data)
-#print(database.readlines()[2])
-database.close()
+#reading file
+def reading_file():
+    database = open("../files/database.txt", "r")
+    for data in database.readlines():
+        print(data)
+    print(database.readlines()[2])
+    database.close()
 
+#appending files
+def appending_file():
+    database = open("../files/database.txt", "a")
+    database.write("\nNike - Shoes")
+    database.close()
+def writing_file():
+    database = open("../files/database_two.txt", "a")
+    database.write("\nNike - Shoes")
+    database.close()
+
+writing_file()
