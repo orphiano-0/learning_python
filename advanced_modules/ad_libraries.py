168c927505aba5c1465ca5ad95687748972b04e4
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-03 15:40:40 +0800

    Added cowsay lib lol

--- advanced_modules/ad_libraries.py
+++ advanced_modules/ad_libraries.py
@@ -1,8 +1,7 @@
 import random
-import sys
+import cowsay
 from prettytable import PrettyTable # You can download package from outside source using pip
 # You can call specific function in a library using syntax 'from' and 'import'
-
 table = PrettyTable(["Names", "Age", "Address", "Status"])
 table.add_row(["Art", 19, "Ashber", "Single"])
 table.add_row(["Tess", 20, "Elshire", "Single"])
@@ -18,3 +17,5 @@ random.shuffle(cards)
 for card in cards:
     print(card)
 
+# cowsay lib
+cowsay.cow("Hello, Gab!")
