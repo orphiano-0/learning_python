5f6635ba189fbd2374fbc6b1a9e63a08492902d2
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-03 16:30:55 +0800

    Improved.

--- advanced_modules/ad_libraries.py
+++ advanced_modules/ad_libraries.py
@@ -1,5 +1,7 @@
+import json
 import random
 import cowsay
+import requests
 from prettytable import PrettyTable # You can download package from outside source using pip
 # You can call specific function in a library using syntax 'from' and 'import'
 table = PrettyTable(["Names", "Age", "Address", "Status"])
@@ -18,4 +20,14 @@ for card in cards:
     print(card)
 
 # cowsay lib
-cowsay.cow("Hello, Gab!")
+cowsay.trex("Hello, Gab!")
+
+# requests lib
+# -- database that has json values
+link = requests.get("https://itunes.apple.com/search?entity=song&limit=20&term=eminem")
+# print(json.dumps(link.json(), indent=2))
+object = link.json()
+# the key 'results' has a value named trackName
+# printing 20(check the limits in url) values of trackName in that list
+for results in object["results"]:
+    print(results["trackName"])
