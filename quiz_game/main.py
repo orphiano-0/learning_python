5cbc39dfdace457a99c956077d51a5d1f7f80126
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-14 20:15:11 +0800

    Adjusted format

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,12 +1,11 @@
 from questions import Questions
 question_prompts = [
-    "Who killed the monster \'Minotaur\' in Greek mythology?\n(a) Hercules\n(b) Theseus\n(c) Hera\n(d) Zeus\n\n",
-    "How many labors does Goddess Hera gave to Hercules?\n(a) 42\n(b) 7\n(c) 12\n(d) 32\n\n",
-    "Who is the God of Underworld?\n(a) Hades\n(b) Persephone\n(c) Dionysus\n(d) Orpheus\n\n",
-    "How many heads does cerberus has?\n(a) 1\n(b) 2\n(c) 3\n(d) None\n\n",
-    "Who killed the serpent snake Orochi?\n(a) Aqua\n(b) Zoro\n(c) Poseidon\n(d) Susa-no-O\n\n"
+    "Who killed the monster \'Minotaur\' in Greek mythology?\n(a) Hercules\n(b) Theseus\n(c) Hera\n(d) Zeus\n",
+    "How many labors does Goddess Hera gave to Hercules?\n(a) 42\n(b) 7\n(c) 12\n(d) 32\n",
+    "Who is the God of Underworld?\n(a) Hades\n(b) Persephone\n(c) Dionysus\n(d) Orpheus\n",
+    "How many heads does cerberus has?\n(a) 1\n(b) 2\n(c) 3\n(d) None\n",
+    "Who killed the serpent snake Orochi?\n(a) Aqua\n(b) Zoro\n(c) Poseidon\n(d) Susa-no-O\n"
 ]
-
 questions = [
     Questions(question_prompts[0], "b"),
     Questions(question_prompts[1], "c"),
@@ -21,6 +20,6 @@ def test(questions):
         answer = input(question.prompts)
         if answer == question.answer:
             score += 1
-    print("Your score is " + str(score) + "/" + str(len(questions)) + " correct")
+    print(f"Your score is {score}/{len(questions)} correct")
 
 test(questions)
