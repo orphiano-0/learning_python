eca1d1ef40d8c81ee83009c9b5c77ec6e4330df5
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-21 19:11:19 +0800

    Add random lib for secret word.

--- basic_program/guessing_game.py
+++ basic_program/guessing_game.py
@@ -1,18 +1,19 @@
 #creating a game for guessing until the user type the secret word
 #-- from codeacedamy
+import random
 
-secret_word = "Humans"
+secret_word = ['tree','sun','ball','moon','earth','grass','world']
 guess = ""
 trial_count = 0
 out_of_guesses = False
-
-while guess != secret_word and not(out_of_guesses):
+sword = random.choice(secret_word)
+while guess != sword and not(out_of_guesses):
     if trial_count < 3:
         guess = input("Enter your guess word: ")
         trial_count += 1
     else:
         out_of_guesses = True
 if out_of_guesses:
-    print("Bah! You suck!")
+    print(f"You lose! The word is {sword}")
 else:
-    print("Congrats!")
+    print(f"You got it! The word is {sword}")
